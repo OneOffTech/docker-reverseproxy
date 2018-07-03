ba93c70e57283117b72fb0d328c610bc489c2ce1
Felix Delattre <github@xama.nu>
2018-07-04 01:00:41 +0200

    Check for variable

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM traefik:1.6-alpine
 
+# Add the bash
+RUN apk add --no-cache bash gawk sed grep bc coreutils
+
 # Apply the standard traefik configuration
 ADD files/traefik.toml /etc/traefik/
 
--- files/start.sh
+++ files/start.sh
@@ -1,19 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-    set -- traefik "$@"
+# Check if required variables are available
+if [ -z "${ADMIN_EMAIL}" ]; then
+    echo "Failure starting Traefik reverse proxy: The environment variable ADMIN_EMAIL must be set."
+else
+
+    # first arg is `-f` or `--some-option`
+    if [ "${1#-}" != "$1" ]; then
+        set -- traefik "$@"
+    fi
+
+    # Populate flexible variable of traefik's configuration from environment variables
+    sed -i "s#{{ADMIN_EMAIL}}#${ADMIN_EMAIL}#" /etc/traefik/traefik.toml
+
+
+    # if our command is a valid Traefik subcommand, let's invoke it through Traefik instead
+    # (this allows for "docker run traefik version", etc)
+    if traefik "$1" --help 2>&1 >/dev/null | grep "help requested" > /dev/null 2>&1; then
+        set -- traefik "$@"
+    fi
+
+    exec "$@"
+
 fi
-
-# Populate flexible variable of traefik's configuration from environment variables
-sed -i "s#{{ADMIN_EMAIL}}#${ADMIN_EMAIL}#" /etc/traefik/traefik.toml
-
-
-# if our command is a valid Traefik subcommand, let's invoke it through Traefik instead
-# (this allows for "docker run traefik version", etc)
-if traefik "$1" --help 2>&1 >/dev/null | grep "help requested" > /dev/null 2>&1; then
-    set -- traefik "$@"
-fi
-
-exec "$@"
